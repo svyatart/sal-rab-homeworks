cb0b939dc1fa4459a84a44d72dd16a48e1a4535b
svyatart <svyatart@mail.ru>
2023-01-10 00:39:34 +0300

    Task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -7,7 +7,23 @@ function parseProducts(json) {
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+
+    JSON.parse(json)
+    let data = JSON.parse(json);
+
+    return data.products;
 }
+
+function renderProductsCards(json){
+
+    clearProducts();
+    products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i += 1) {
+        addProduct(products[i]);
+    }
+
+};
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
