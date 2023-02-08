14784e10b598b94a5fc9aa8a76a11ce3cd2f2bba
svyatart <svyatart@mail.ru>
2023-02-09 00:36:22 +0300

    Task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,9 +39,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        
-       data.goods.push({title: goods[i].title, count: goods[i].count});
-
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
     data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
